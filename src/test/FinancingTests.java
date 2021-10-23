42cfdebcb7b50ba8c1e8ae2e07daf1a5b4771777
Rômulo Albuquerque <66214784+RomuloAlbuquerque@users.noreply.github.com>
2021-10-23 13:36:57 -0300

    Created test method setIncomeShouldUpdateIncomeWhenValidData()

--- src/test/FinancingTests.java
+++ src/test/FinancingTests.java
@@ -9,13 +9,15 @@ import test.factory.FinancingFactory;
 public class FinancingTests {
 	
 	final double VALID_TOTAL_AMOUNT = 100000.00;
-	final double VALID_INCOME = 2000.00;
-	final int VALID_MONTHS = 80;
-	
 	final double VALID_SET_TOTAL_AMOUNT = 99999.99;
 	final double INVALID_TOTAL_AMOUNT = 1000000.00;
 	
+	final double VALID_INCOME = 2000.00;
+	final double VALID_SET_INCOME = 2000.01;
+	
+	final int VALID_MONTHS = 80;
 	final int INVALID_MONTHS = 20;
+
 	
 	@Test
 	public void constructorShouldCreateObjectsCorrectlyWhenValidData() {
@@ -57,6 +59,18 @@ public class FinancingTests {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			f.setTotalAmount(INVALID_TOTAL_AMOUNT);
 		});
+	}
+	
+	@Test
+	public void setIncomeShouldUpdateIncomeWhenValidData() {
+		//arrange
+		Financing f = FinancingFactory.createFinancingWithValidDatas();
+		
+		//action
+		f.setIncome(VALID_SET_INCOME);
+		
+		//assert
+		Assertions.assertEquals(f.getIncome(), VALID_SET_INCOME);
 	}
 
 }
